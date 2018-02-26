2ee56bb9c9e8295a11d0d94a096822e2047aafdd
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2018-02-26 16:50:22 +0200

    Release 1.0.7

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,6 @@ module.exports = {
   plugins: [
     new PeerDepsExternalsPlugin(),
     new webpack.IgnorePlugin(/node_modules$/),
-    new WebpackBundleAnalyzer(),
+    // new WebpackBundleAnalyzer(),
   ],
 };
